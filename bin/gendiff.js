954f64335bc350b4614c2bf23e9c4e2718b34e32
Marina Kucheryavenko <kmskms84@mail.ru>
2023-01-14 01:56:50 +0300

    fix gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,15 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2) => {
+    //   const diff = genDiff(filepath1, filepath2);
+    //   console.log(diff);
+  });
 
 program.parse();
