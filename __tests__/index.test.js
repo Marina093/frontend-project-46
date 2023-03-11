4f84cf0e38a342fae9fd454137d04d884b1f3420
Marina Kucheryavenko <kmskms84@mail.ru>
2023-03-11 14:35:00 +0300

    add import test,expect in index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,6 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
+import { expect, test } from '@jest/globals';
 import genDiff from '../src/index.js';
 import expected from '../__fixtures__/expected.js';
 
@@ -8,5 +9,5 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 test('genDiff', () => {
-	  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(expected);
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(expected);
 });
