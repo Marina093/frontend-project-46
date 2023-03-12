02e2a60098be8f3c796262b712c62e8e5ba55310
Marina Kucheryavenko <kmskms84@mail.ru>
2023-03-12 21:44:00 +0300

    fix src/treeDiff.js

--- src/treeDiff.js
+++ src/treeDiff.js
@@ -4,8 +4,10 @@ const getTreeDiff = (file1, file2) => {
   const keys1 = _.keys(file1);
   const keys2 = _.keys(file2);
   const keys = _.union(keys1, keys2);
+
   const sortedKeys = _.sortBy(keys);
-  const resultTree = sortedKeys.map((key) => {
+
+  return sortedKeys.map((key) => {
     if (!_.has(file1, key)) {
       return { keyName: key, type: 'added', value2: file2[key] };
     }
@@ -20,8 +22,7 @@ const getTreeDiff = (file1, file2) => {
         keyName: key, type: 'changed', value1: file1[key], value2: file2[key],
       };
     }
-    return { keyName: key, type: 'unchanged', value: file1[key] };
+    return { keyName: key, type: 'unchanged', value: file2[key] };
   });
-  return resultTree;
 };
 export default getTreeDiff;
