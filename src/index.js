d854b3e27c2d8867434325d6338b321123032ba2
Marina Kucheryavenko <kmskms84@mail.ru>
2023-03-19 21:16:14 +0300

    add format

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import { readFileSync } from 'fs';
 import path from 'path';
 import parsers from './parsers.js';
 import getTreeDiff from './treeDiff.js';
-import stylish from './stylish.js';
+import formatSelection from './formatters/index.js';
 
 const readFile = (filepath) => readFileSync(filepath, 'utf-8');
 const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
 const getFormat = (filepath) => path.extname(filepath);
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const data1 = readFile(getAbsolutePath(filepath1));
   const data2 = readFile(getAbsolutePath(filepath2));
 
@@ -16,6 +16,6 @@ const genDiff = (filepath1, filepath2) => {
   const dataParse2 = parsers(data2, getFormat(filepath2));
 
   const difference = getTreeDiff(dataParse1, dataParse2);
-  return stylish(difference);
+  return formatSelection(difference, formatName);
 };
 export default genDiff;
