3022615d81d1397cec8d31a656c7f71aede55d7c
Marina Kucheryavenko <kmskms84@mail.ru>
2023-03-22 22:16:21 +0300

    add destructuring

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -5,37 +5,40 @@ const stringify = (data, depth) => {
   if (typeof data !== 'object' || data === null) {
     return `${data}`;
   }
-  const strArray = Object.entries(data).map(([key, value]) => `${' '.repeat(spacesCount * depth + 2)}  ${key}: ${stringify(value, depth + 1)}`);
-  const finalString = `{\n${strArray.join('\n')}\n${' '.repeat(spacesCount * depth)}}`;
-  return finalString;
+  const indent = ' '.repeat(spacesCount * depth + 2);
+  const strArray = Object
+    .entries(data)
+    .map(([key, value]) => `${indent}  ${key}: ${stringify(value, depth + 1)}`);
+  return `{\n${strArray.join('\n')}\n${' '.repeat(spacesCount * depth)}}`;
 };
 
 const stylish = (data) => {
   const iter = (node, depth) => {
-    const name = node.keyName;
-    switch (node.type) {
+    const {
+      keyName, type, value1, value2,
+    } = node;
+    switch (type) {
       case 'nested': {
         const arr = node.children.flatMap((child) => iter(child, depth + 1));
-        return `${getIndent(depth)}  ${name}: {\n${arr.join('\n')}\n${getIndent(depth)}  }`;
+        return `${getIndent(depth)}  ${keyName}: {\n${arr.join('\n')}\n${getIndent(depth)}  }`;
       }
       case 'added':
-        return `${getIndent(depth)}+ ${name}: ${stringify(node.value2, depth)}`;
+        return `${getIndent(depth)}+ ${keyName}: ${stringify(value2, depth)}`;
 
       case 'deleted':
-        return `${getIndent(depth)}- ${name}: ${stringify(node.value1, depth)}`;
+        return `${getIndent(depth)}- ${keyName}: ${stringify(value1, depth)}`;
 
       case 'changed': {
-        const str1 = `${getIndent(depth)}- ${name}: ${stringify(node.value1, depth)}`;
-        const str2 = `${getIndent(depth)}+ ${name}: ${stringify(node.value2, depth)}`;
+        const str1 = `${getIndent(depth)}- ${keyName}: ${stringify(value1, depth)}`;
+        const str2 = `${getIndent(depth)}+ ${keyName}: ${stringify(value2, depth)}`;
         return [str1, str2];
       }
       default:
-        return `${getIndent(depth)}  ${name}: ${stringify(node.value, depth)}`;
+        return `${getIndent(depth)}  ${keyName}: ${stringify(node.value, depth)}`;
     }
   };
   const resultArray = data.flatMap((node) => iter(node, 1));
-  const resultString = `{\n${resultArray.join('\n')}\n}`;
-  return resultString;
+  return `{\n${resultArray.join('\n')}\n}`;
 };
 
 export default stylish;
